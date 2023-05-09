271599d6a33565f58177c803478c765fbc1c1c36
ikozigbo <97337028+ikozigbo@users.noreply.github.com>
2023-05-09 18:16:47 +0100

    09/05

---
+++ src/post.js
@@ -0,0 +1,22 @@
+const Post = () => {
+  return (
+    <div className="post">
+      <div className="image">
+        <img src="images/world.jpg" alt="jsja" />
+      </div>
+      <div className="texts">
+        <h2>Full-house batter backup coming later this year</h2>
+        <p className="info">
+          <a className="author">Ikponmwosa</a>
+          <time>2023-01-06 16:45</time>
+        </p>
+        <p className="summary">
+          lorem djfhfjdk ddncjd jfhdhdj jdndncn kk jfnj mdmi nnghi dujf dmsm
+          lorem djfhfjdk ddncjd jfhdhdj jdndncn kk jfnj mdmi nnghi dujf dmsm
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
