64fe69c4b542b8c79dff0944963988ef6fd0e63a
ikozigbo <97337028+ikozigbo@users.noreply.github.com>
2023-05-17 10:12:55 +0100

    17/05

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,38 +1,39 @@
 import { useState } from "react";
 
-export default function LoginPage() {
+export default function RegisterPage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  async function login(ev) {
+  async function register(ev) {
     ev.preventDefault();
-    await fetch("http://localhost:4000/login", {
+    const response = await fetch("http://localhost:4000/register", {
       method: "POST",
       body: JSON.stringify({ username, password }),
       headers: { "Content-Type": "application/json" },
     });
+    if (response.status === 200) {
+      alert("registration good");
+    } else {
+      alert("registration failed");
+    }
   }
 
   return (
-    <form className="login" onSubmit={login}>
-      <h1>Login</h1>
+    <form className="register" onSubmit={register}>
+      <h1>Register</h1>
       <input
         type="text"
         placeholder="username"
         value={username}
-        onChange={(ev) => {
-          setUsername(ev.target.value);
-        }}
+        onChange={(ev) => setUsername(ev.target.value)}
       />
       <input
         type="password"
         placeholder="password"
         value={password}
-        onChange={(ev) => {
-          setPassword(ev.target.value);
-        }}
+        onChange={(ev) => setPassword(ev.target.value)}
       />
-      <button>Login</button>
+      <button>Register</button>
     </form>
   );
 }
