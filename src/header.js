105e3e9fba7b7c12b6eec04dda1ffcaec6ab9730
ikozigbo <97337028+ikozigbo@users.noreply.github.com>
2023-05-18 19:05:48 +0100

    18/05

--- src/header.js
+++ src/header.js
@@ -1,6 +1,12 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  useEffect(() => {
+    fetch("http://localhost:4000/profile", {
+      credentials: "include",
+    });
+  }, []);
   return (
     <header>
       <Link to="/" className="logo">
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,16 +1,28 @@
 import { useState } from "react";
+import { Navigate } from "react-router-dom";
 
 export default function LoginPage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
 
   async function login(ev) {
     ev.preventDefault();
-    await fetch("http://localhost:4000/login", {
+    const response = await fetch("http://localhost:4000/login", {
       method: "POST",
       body: JSON.stringify({ username, password }),
       headers: { "Content-Type": "application/json" },
+      credentials: "include",
     });
+    if (response.ok) {
+      setRedirect(true);
+    } else {
+      alert("wrong credentials");
+    }
+  }
+
+  if (redirect) {
+    return <Navigate to={"/"} />;
   }
 
   return (
