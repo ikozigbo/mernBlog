db30b965bd3247d7bd58a75cfb7aee37dc11fe5e
ikozigbo <97337028+ikozigbo@users.noreply.github.com>
2023-05-20 21:55:03 +0100

    20/05

--- src/App.js
+++ src/App.js
@@ -7,17 +7,20 @@ import Layout from "./layout";
 import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import { UserContextProvider } from "./UserContext";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<IndexPage />} />
-          <Route path={"/login"} element={<LoginPage />} />
-          <Route path={"/register"} element={<RegisterPage />} />
-        </Route>
-      </Routes>
+      <UserContextProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<IndexPage />} />
+            <Route path={"/login"} element={<LoginPage />} />
+            <Route path={"/register"} element={<RegisterPage />} />
+          </Route>
+        </Routes>
+      </UserContextProvider>
     </>
   );
 }
