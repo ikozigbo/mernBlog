af19d895275590d0681f3c0f0b804806a6b7ddfb
ikozigbo <97337028+ikozigbo@users.noreply.github.com>
2023-06-04 20:47:35 +0100

    o4/06

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import { UserContextProvider } from "./UserContext";
 import CreatePost from "./pages/CreatePost";
+import PostPage from "./pages/PostPage";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
             <Route path="/create" element={<CreatePost />} />
+            <Route path="/post/:id" element={<PostPage />} />
           </Route>
         </Routes>
       </UserContextProvider>
---
+++ src/pages/PostPage.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+export default function PostPage() {
+  const { postInfo, setPostInfo } = useState(null);
+  const { id } = useParams();
+  useEffect(() => {
+    //console.log(params);
+    fetch(`http://localhost:4000/post/${id}`).then((response) => {
+      response.json().then((postInfo) => {
+        setPostInfo(postInfo);
+      });
+    });
+  }, []);
+
+  return;
+}
